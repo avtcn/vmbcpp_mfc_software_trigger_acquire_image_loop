86873857d9f9d2ea51f59dd2f2cf6d9b02cab717
joege <joe.ge@alliedvision.com>
2020-08-31 17:12:16 +0800

    Vimba 4.0 C++ MFC/Dialog program to test Async/Sync photo taking modes in loop.

---
+++ Source/res/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 AsynchronousGrab.rc 使用
+//
+#define IDD_ASYNCHRONOUSGRAB_DIALOG     102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST_CAMERAS                1000
+#define IDC_BUTTON_STARTSTOP            1001
+#define IDC_LIST_LOG                    1006
+#define IDC_PICTURE_STREAM              1007
+#define IDC_BUTTON_OPEN_CAMERA          1008
+#define IDC_BUTTON_SW_TRIGGER           1009
+#define IDC_BUTTON_CLOSE_CAMERA         1010
+#define IDC_BUTTON_SW_TRIGGER_AUTO      1011
+#define IDC_BUTTON2                     1012
+#define IDC_BUTTON_CLOSE_CAM_SYNC       1012
+#define IDC_BUTTON_OPEN_CAM_SYNC        1013
+#define IDC_BUTTON_SYNC_ACQUIRE_IMG     1014
+#define IDC_BUTTON4                     1015
+#define IDC_BUTTON_ACQUIRE_IMG_AUTO     1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
